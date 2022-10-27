0dcd03a51293584eef6f1a176c43152e0fd27095
BeyondDesolation <lightnight19@yandex.ru>
2022-10-27 23:11:29 +0400

    Подготовка главного окна и контролов для него

---
+++ D.IBlab1/ViewModels/UserControlsViewModels/WelcomeControlViewModel.cs
@@ -0,0 +1,13 @@
+﻿using D.IBlab1.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D.IBlab1.ViewModels.UserControlsViewModels
+{
+    internal class WelcomeControlViewModel : ViewModelBase
+    {
+    }
+}
