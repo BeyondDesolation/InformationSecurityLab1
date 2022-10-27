6dd052a6de95d19a9d35f683da0cb416bf8fb3fd
BeyondDesolation <lightnight19@yandex.ru>
2022-10-27 21:23:50 +0400

    Добавьте файлы проекта.

---
+++ D.IBlab1/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using D.IBlab1.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D.IBlab1.ViewModels
+{
+    internal class MainWindowViewModel : ViewModelBase
+    {
+        private string _title = "";
+        public string Title
+        {
+            get => _title;
+            set => Set(ref _title, value);
+        }
+    }
+}
